f401f4292f7b94fafa9f3503709405153e17fdef
arianxa <pro.arantxa.ordoyo@gmail.com>
2020-04-23 18:04:56 +0200

    d
    s

--- ejercicio1/node-api/routes/users.js
+++ ejercicio1/node-api/routes/users.js
@@ -16,5 +16,51 @@ router.get('/', function(req, res, next) {
 });
 
 
+
+router.post('/alta_Registro', function(req, res, next) {
+  var registro={
+    Nombre:req.body.Nombre,
+    Apellidos:req.body.Apellidos,
+    Email:req.body.Email,
+    Contraseña:req.body.Contraseña    
+    };
+  //  var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+    connection.query('insert into bd_web.registro set ?',registro, function (error,resultado){
+      if(error){
+        res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+        //Hay un error a la hora de conectarse a la BBDD
+    } else {
+        res.send(JSON.stringify({"status": 201, "error": null, "response": resultado}));
+        //Se envian todos los usuarios
+    }
+  });    
+});
+
+
+
+
+router.post('/alta', function(req, res, next) {
+  var registro={
+      Nombre:req.body.Nombre,
+      URL_Foto:req.body.URL_Foto
+    };
+    var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+
+    connection.query('insert into bd_web.productos set ?',registro, function (error,resultado){
+      if(error){
+        res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+        //Hay un error a la hora de conectarse a la BBDD
+    } else {
+        res.send(JSON.stringify({"status": 201, "error": null, "response": resultado}));
+        //Se envian todos los usuarios
+    }
+  });    
+});
+
+
+
+
+
+
 // 
 module.exports = router;
